4e8374aafbfbcbc1432f731973dd7235712bb969
chingan <chingan@192.168.11.121>
2017-09-07 13:54:28 +0800

    序列和 給定一個序列，第n個元素 Tn = n2 - (n-1)2。 求序列的和 Sn % (109 + 7)的結果。 其中  Sn = T1 + T2 + T3 + .... + Tn。 輸入格式如下： 第一行包含一個整數「N」，代表測試數據的個數。 每個測試數據對應一行，包含一個整數「n」。 輸出格式： 對每一個測試數據，用一行輸出Sn % (109 + 7)。 數據範圍： 1 <= N <= 10 1 <= n <= 1016 範例輸入： 2 2 1 範例輸出： 4 1 題目1： 請用遞迴的方式撰寫上述題目，並請在檔案開頭用註解的方式分析時間複雜度與空間複雜度。 題目2： 請重新撰寫此程式，同時在時間複雜度與空間複雜度必須皆為O(1)。
    註1：請注意本題的數字範圍，你有可能需要使用型態long
    註2：請注意本題的數字範圍，你有可能需要應用模數運算的分配律，如下：
    假設有一個整數K與n，則
    K2 mod n = [(K mod n) (K mod n)] mod n

--- constructure1_complexity/src/constructure1_complexity/Test.java
+++ constructure1_complexity/src/constructure1_complexity/Test.java
@@ -1,9 +1,44 @@
 package constructure1_complexity;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		int n;
+		long[] inputs;
+		Scanner scanner = new Scanner(System.in);
+		
+		while (true) {
+			inputs = new long[10];
+			System.out.println("Input number of inputs(-1 quit):");
+			n = scanner.nextInt();
+			if (n == -1)
+				break;
+			System.out.println("Inputs:");
+			for (int i = 0; i < n; i++)
+				inputs[i] = scanner.nextLong();
+			System.out.println("Results:");
+			for (int i = 0; i < n; i++)
+				System.out.println(modResult(sumRecur(inputs[i])));
+		}
 
 	}
-
+	
+	
+	public static long sumRecur(long n) {
+		if (n == 1) {
+			return 1;
+		}
+		else {
+			return (n * 2 - 1 + sumRecur(n - 1));
+		}
+	}
+	
+	public static long modResult(long n) {
+		long l =  (n % (long)(Math.pow(10, 9) + 7));
+		return l;
+	}
+	
 }
