e3f4c96e522f710a75041416ff51ad6cebfa3be9
chingan <chingan@192.168.11.121>
2017-09-07 16:26:59 +0800

    Finished.

--- constructure1_complexity/src/leftRotation/LeftRotate.java
+++ constructure1_complexity/src/leftRotation/LeftRotate.java
@@ -1,10 +1,42 @@
 package leftRotation;
 
+import java.util.Scanner;
+
 public class LeftRotate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		int[] intArr = {1, 2, 3, 4, 5, 6, 7, 10};
+		Scanner scanner = new Scanner(System.in);
+		int[] intArr;
+		int n, d;
+		System.out.println("Number and displacement:");
+		n = scanner.nextInt();
+		d = scanner.nextInt();
+		intArr = new int[n];
+		for (int i = 0; i < n; i++)
+			intArr[i] = scanner.nextInt();
+		
+		leftRotation(intArr, d);
+		for (int i = 0; i < n; i++)
+			System.out.print(intArr[i]+" ");
+		
+		System.out.println("\nProgram terminate.");
 
+	}
+	
+	public static void leftRotation(int[] arr, int d) {
+		int count = arr.length, tmp = arr[count - d];
+		int oldInd = 0, newInd = count - d;
+		
+		for (int i = 0; i < count - 1; i++) {
+			arr[newInd] = arr[oldInd];
+			newInd = oldInd;
+			if (oldInd + d < count)
+				oldInd += d;
+			else
+				oldInd = oldInd + d - count;
+		}
+		arr[newInd] = tmp;
 	}
 
 }
